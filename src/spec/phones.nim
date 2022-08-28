142094b6b40fb5192905c80a8a63496b5635f723
N545PY <nsaspy@airmail.cc>
2022-08-28 14:42:56 -0400

    removed more optional types

--- src/spec/phones.nim
+++ src/spec/phones.nim
@@ -1,13 +1,12 @@
 import documents
-import options
 type
   BookerPhone* = ref object of BookerDocument
       ## A Phone number
       phone*: string
-      carrier*: Option[string]
-      status*: Option[string]
-      phone_type*: Option[string]
+      carrier*: string
+      status*: string
+      phone_type*: string
 
 
-proc newPhone*(phone: string, carrier, status, phone_type = none(string)): BookerPhone =
+proc newPhone*(phone: string, carrier, status, phone_type = ""): BookerPhone =
   BookerPhone(phone: phone, carrier: carrier, status: status, phone_type: phone_type)
