055497680f58f233b57690bd0d954030397b0e57
N545PY <nsaspy@airmail.cc>
2022-09-01 14:02:52 -0400

    removed memberships type. may or may not return

--- src/spec/documents.nim
+++ src/spec/documents.nim
@@ -1,27 +1,15 @@
 import std/[hashes]
 import uuids
-# TODO whendifed fast is using uuids, which is slow, flip them
 
 type
     BookerDocument* = ref object of RootObj
       ## Base Object to hold the document metadata thats used to make a dcoument and store it in couchdb
-      ## _id and _rev are renamed to id and rev due to limitations of nim.
       id*: string
       source_dataset*: string
       dataset*: string
       dtype*: string
       date_added*: string
       date_updated*: string
-
-    BookerMembership* = ref object of BookerDocument
-      ## Document holding metadata linking two documents
-      relation_type*: string
-      title*: string
-      roles*: seq[string]
-      start_date*: string
-      end_date*: string
-      child*: string
-      parent*: string
 
 
 proc makeHash*(input: string): string =
