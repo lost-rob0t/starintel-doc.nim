d67b34e3f4ddc293e2f040586aad192c72e97071
nsaspy <nsaspy@airmail.cc>
2022-12-26 18:44:27 -0500

    add docstrings

--- src/spec/relation.nim
+++ src/spec/relation.nim
@@ -1,4 +1,5 @@
 import documents
+import documents
 type
   Relations* = enum
     to = 1
@@ -7,15 +8,29 @@ type
     child = 4
     between = 5
     member = 6
-  BookerRelation* = ref object
-    id*: string
-    rev*: string
-    relation*: Relations
-    source*: string
-    target*: string
 
-
+type BookerRelation* = ref object
+  ## A object that represents a relationship between two entities
+  id*: string
+  ## The ID of the `BookerRelation` object
+  rev*: string
+  ## The revision of the `BookerRelation` object
+  relation*: Relations
+  ## The type of the relationship
+  source*: string
+  ## The ID of the source entity
+  target*: string
+  ## The ID of the target entity
 proc newRelation*(source, target: string, relation: Relations): BookerRelation =
+  ## Creates a new `BookerRelation` object with the specified `source`, `target`, and `relation` values
+  ##
+  ## Parameters:
+  ##   source (string): The ID of the source entity
+  ##   target (string): The ID of the target entity
+  ##   relation (Relations): The type of the relationship
+  ##
+  ## Returns:
+  ##   BookerRelation: The newly created `BookerRelation` object
   var doc = BookerRelation(source: source, target: target, relation: relation)
   doc.makeUUID
   result = doc
