f2a40f9b53a804aa1c4a4b4b31326192142d666d
nsaspy <nsaspy@airmail.cc>
2023-04-04 18:05:28 -0400

    add remaining tests

--- tests/test_spec/test_base.nim
+++ tests/test_spec/test_base.nim
@@ -1,8 +1,7 @@
-import ../../src/starintel_doc/documents
+import ../../src/starintel_doc/[documents, targets, relation]
 import times
 proc testBaseDocument() =
   let time = now().toTime.toUnix
-  echo "time is, ", $time
   var doc = BookerDocument(dataset: "Tests",
                             dtype: "test_doc", date_added: time, date_updated: time, id: "test")
   doAssert doc.dataset == "Tests"
@@ -10,3 +9,30 @@ proc testBaseDocument() =
   doAssert doc.date_added == time
   doAssert doc.date_updated == time
   doAssert doc.id == "test"
+
+
+
+proc testBookerTarget() =
+  let target = "nsaspy"
+  let dataset = "git accounts"
+  let actor = "GitBot"
+  var doc = newTarget(dataset, target, actor)
+  assert doc.target == target
+  assert doc.dataset == dataset
+  assert doc.actor == actor
+proc testRelation() =
+  let sourceId = "testfoobar"
+  let targetId = "testbarfoo"
+  var doc = newRelation(source=sourceId, target=targetId, relation = Relations.to, dataset = "test")
+  assert doc.source == sourceId
+  assert doc.target == targetId
+  assert doc.relation == Relations.to
+
+
+when isMainModule:
+  echo "testing: BookerDocument"
+  testBaseDocument()
+  echo "testing: BookerTarget"
+  testBookerTarget()
+  echo "testing: BookerRelation"
+  testRelation()
