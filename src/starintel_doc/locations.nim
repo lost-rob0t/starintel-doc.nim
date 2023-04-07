60bd861c184dbcfa17a8468cb35b4d5fee8d6d58
nsaspy <nsaspy@airmail.cc>
2023-04-06 21:18:25 -0400

    Make BookerGeo inherit from BookerDcoument
    Documents are now "linked" instead of embedded in the doc
    Document ids should really never change so this should be fine

--- src/starintel_doc/locations.nim
+++ src/starintel_doc/locations.nim
@@ -2,11 +2,10 @@ import documents
 import options
 import strutils
 type
-  BookerGeo* = ref object of RootObj
+  BookerGeo* = ref object of BookerDocument
     lat*: float64
     long*: float64
     alt*: float64
-    gid*: string
 
   BookerAddress* = ref object of BookerGeo
     city*: string
@@ -18,5 +17,5 @@ type
 
 proc newAddress*(street, street2, city, postal, state, country: string, lat, long = 0.0): BookerAddress =
   var doc = BookerAddress(street: street, street2: street2, city: city, postal: postal, state: state, country: country, lat: lat, long: long)
-  doc.gid = makeHash(street & street2 & city & state & country & postal)
+  doc.makeMD5ID(street & street2 & city & state & country & postal)
   result = doc
