6fbff2523862b944ef042d98d29c8f7d7ab12a9e
nsaspy <nsaspy@fedora.email>
2023-09-10 18:32:55 -0400

    add space

--- src/starintel_doc/documents.nim
+++ src/starintel_doc/documents.nim
@@ -3,6 +3,8 @@ import uuids
 from times import getTime, toUnix
 export getTime, toUnix
 import json
+
+
 type
     Document* = ref object of RootObj
       ## Base Object to hold the document metadata thats used to make a dcoument and store it in couchdb
@@ -18,13 +20,16 @@ proc makeHash*(input: string): string =
   when defined(debug):
     echo $result
 
+
 template link*[T,V](doc: T, field: untyped, data: V) =
   field.add(data)
+
 
 
 template makeUUID*[T](doc: T) =
   ## Generate a UUID for a document
   doc.id = $genUUID()
+
 
 template makeEID*[T](doc: T, data: string) =
   ## Generate a EID
@@ -33,13 +38,16 @@ template makeEID*[T](doc: T, data: string) =
   doc.eid = makeHash(data)
 
 
+
 template makeMD5ID*[T](doc: T, data: string) =
   ## Generate a MD5 checksum for the document id
   doc.id = $toMD5(data)
 
+
 template makeSHAID*[T](doc: T, data: string) =
   ## Generate a SHA1 checksume for the document ID
   doc.id = $secureHash(data)
+
 
 template timestamp*[T](doc: T) =
   ## Add a timestamp to the document
@@ -48,17 +56,18 @@ template timestamp*[T](doc: T) =
   doc.date_added = t.toUnix()
   doc.date_updated = t.toUnix()
 
+
 template updateTime*[T](doc: T) =
   ## update the date_updated timestamp to the document
   let t = getTime()
   doc.date_updated = t.toUnix()
+
 
 proc dump*[T](doc: T): JsonNode =
   var jdoc = %*doc
   jdoc{"_id"} = newJString(doc.id)
   jdoc.delete("id")
   result = jdoc
-
 
 
 proc load*[T](node: JsonNode, t: typedesc[T]): T =
