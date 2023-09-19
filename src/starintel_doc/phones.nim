c220d840cd62ee9a6d37353d5aea915553d7ec7b
nsaspy <nsaspy@fedora.email>
2023-09-19 11:00:08 -0400

    Phone.phoner => Phone.number
    All these renames are in a goal to improve the style and quality of the spec

--- src/starintel_doc/phones.nim
+++ src/starintel_doc/phones.nim
@@ -2,11 +2,11 @@ import documents
 type
   Phone* = ref object of Document
       ## A Phone number
-      phone*: string
+      number*: string
       carrier*: string
       status*: string
       phone_type*: string
 
 
 proc newPhone*(phone: string, carrier, status, phone_type = ""): Phone =
-  Phone(phone: phone, carrier: carrier, status: status, phone_type: phone_type, dtype: "phone")
+  Phone(number: phone, carrier: carrier, status: status, phone_type: phone_type, dtype: "phone")
